8449a23a0c64b9c8b91869d3c14181be5bce5cce
Paul Kennedy <pkkenne.dev@gmail.com>
2026-05-10 05:16:50 -0400

    Remove toolbar legacy icons

--- src/qtdisplay/chart/view/toolbar.py
+++ src/qtdisplay/chart/view/toolbar.py
@@ -6,8 +6,8 @@ from PyQt6.QtGui import QColor, QAction
 from PyQt6.QtWidgets import QApplication, QToolBar
 
 from qtdisplay.chart.view.dialog import (ChartDisplaySettings)
+from qtgui.icons import _get_line_icon
 
-from cross_platform.dev.icons_legacy.svg_path import get_icon, IconType, IconManager
 from qtgui.style.toolbar import StyledToolBar
 
 
@@ -39,20 +39,20 @@ class ChartToolBar(StyledToolBar):
     def _setup_ui(self):
         """Set up the toolbar UI."""
         # Chart snapshot
-        snapshot_icon = get_icon(IconType.CAMERA_LENS, self.iconSize())
+        snapshot_icon = _get_line_icon("camera-lens", self.iconSize())
         self.snapshot_action = QAction(snapshot_icon, "Snapshot", self)
         self.snapshot_action.setToolTip("Save an image of the chart")
         self.addAction(self.snapshot_action)
 
         # Chart full view
-        fullscreen_icon = get_icon(IconType.WINDOW_MAXIMIZE, self.iconSize())
+        fullscreen_icon = _get_line_icon("fullscreen", self.iconSize())
         self.full_screen_action = QAction(fullscreen_icon, "Full View", self)
         self.full_screen_action.setToolTip("Show or hide chart axes and labels")
         self.full_screen_action.setCheckable(True)
         self.addAction(self.full_screen_action)
 
         # Chart table
-        table_icon = get_icon(IconType.TABLE, self.iconSize())
+        table_icon = _get_line_icon("table", self.iconSize())
         self.table_action = QAction(table_icon, "Data Table", self)
         self.table_action.setToolTip("Show or hide chart data table")
         self.table_action.setCheckable(True)
@@ -60,17 +60,17 @@ class ChartToolBar(StyledToolBar):
 
         self.addSeparator()
         # Chart settings
-        settings_icon = get_icon(IconType.SETTINGS, self.iconSize())
+        settings_icon = _get_line_icon("settings", self.iconSize())
         self.settings_action = QAction(settings_icon, "Settings", self)
         self.settings_action.setToolTip("Show chart settings menu")
         self.addAction(self.settings_action)
 
         self.icon_map = defaultdict(None)
         self.icon_map.update({id(
-            self.full_screen_action):IconType.WINDOW_MINIMIZE,
-             id(self.table_action): IconType.TABLE,
-             id(self.settings_action): IconType.SETTINGS,
-             id(self.snapshot_action): IconType.CAMERA_LENS})
+            self.full_screen_action):"fullscreen-exit",
+             id(self.table_action): "table",
+             id(self.settings_action): "settings",
+             id(self.snapshot_action): "camera-lens"})
 
     def get_text_color(self):
         app = QApplication.instance()
@@ -94,14 +94,13 @@ class ChartToolBar(StyledToolBar):
     def _on_fullscreen_toggle(self, checked: bool):
         """"""
         self._isfullscreen = checked
-        IconManager.clear_cache()
         if checked:
-            self.full_screen_action.setIcon(get_icon(IconType.WINDOW_MINIMIZE,
+            self.full_screen_action.setIcon(_get_line_icon("fullscreen-exit",
                                                      self.iconSize(),
                                                      self.get_text_color()))
         else:
             self.full_screen_action.setIcon(
-                get_icon(IconType.WINDOW_MAXIMIZE, self.iconSize(),
+                _get_line_icon("fullscreen", self.iconSize(),
                          self.get_text_color()))
 
         self.fullViewRequested.emit(checked)
